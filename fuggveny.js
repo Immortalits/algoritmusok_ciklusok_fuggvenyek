31c29e5076ea24d236e316b011d769630e382b22
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-05-09 08:47:39 +0200

    array push majdnem kész

---
+++ fuggveny.js
@@ -0,0 +1,21 @@
+const x = ['', 4, true];
+for (let i = 0; i < x.length; i++) {
+  let a = x[i];
+  const f = [];
+  f.push(typeof (a)); if (i < x.length) {
+    console.log(f)
+  }
+}
+
+// const a = x[i];
+// const b = x[1];
+// const c = x[2];
+
+// const f = [];
+
+// f.push(typeof (a));
+// f.push(typeof (b));
+// f.push(typeof (c));
+
+// console.log(f)
+
