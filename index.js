59539ea59754d8f8360a04cacfa9bd7cf6e57a4b
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-05-10 08:11:20 +0200

    még több, comment hozzáadva

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// létrehozzuk a players array-t
 const players = ['Peter', 'Kate', 'John'];
 
+// for-on belül létrehozzuk az i változót, ami 0-tól a players hosszáig (elemeinek mennyiségéig) megy egyesével
 for (let i = 0; i < players.length; i++) {
+  // consolra kiírjuk a nums elemének sorszámás+1 -mert 0-tól kezdődik a számozás, de mi 1-től sorszámozunk - hozzáfűzünk egy '. ' stringet és és hozzáfűzzük az elemet is
   console.log([i + 1] + '. ' + players[i])
 }
--- oszthato.js
+++ oszthato.js
@@ -1,9 +1,14 @@
+// létrehozzuk a nums array-t
 const nums = [3, 4, 9, 6, 2];
 
+// for-on belül létrehozzuk az i változót, ami 0-tól a nums hosszáig (elemeinek mennyiségéig) megy egyesével
 for (let i = 0; i < nums.length; i++) {
+  // ha a nums adott eleme 2-vel osztva nem ad maradékot, akkor...
   if (nums[i] % 2 === 0) {
+    // ...akkor kiírjuk a console-ra az elemet + ': osztható' stringet
     console.log(nums[i] + ': osztható')
   } else {
+    // ...ha ad maradékot, akkor kiírjuk a console-ra az elemet + ': nem osztható' stringet
     console.log(nums[i] + ': nem osztható')
   }
 }
