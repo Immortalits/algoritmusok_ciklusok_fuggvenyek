8fb2ca27d6ed4130c21ded48a3b147fe8c73eb16
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-05-10 08:04:05 +0200

    extra sorok törölve, commentek hozzáadva

--- fuggveny.js
+++ fuggveny.js
@@ -1,21 +1,14 @@
+// létrehozzuk az x array-t, melynek elemei: üres - String, 4 - szám, true -boolean
 const x = ['', 4, true];
+// létrehozzuk az f array-t, ami üres
 const f = [];
 
+// for-on belül létrehozzuk az i változót, ami 0-tól az x hosszáig (elemeinek mennyiségéig) megy egyesével
 for (let i = 0; i < x.length; i++) {
+  // létrehozzuk az a változót, ami az x i-dik eleme
   let a = x[i];
+  // beletesszük az a változó típusát az f array-e
   f.push(typeof (a));
 }
+// kiírjuk a console-ra az f arrayt
 console.log(f)
-
-// const a = x[i];
-// const b = x[1];
-// const c = x[2];
-
-// const f = [];
-
-// f.push(typeof (a));
-// f.push(typeof (b));
-// f.push(typeof (c));
-
-// console.log(f)
-
